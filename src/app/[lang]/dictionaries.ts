a3d3c3d6ae61ba0c670e59e4e1c05d012a2cb2e9
yuuki008 <s22ah261@gmail.com>
2025-11-15 08:57:25 +0900

    Delete language dictionaries and refactor page content to use hardcoded data

--- src/app/[lang]/dictionaries.ts
+++
@@ -1,9 +0,0 @@
-import "server-only";
-
-const dictionaries = {
-  en: () => import("./dictionaries/en.json").then((module) => module.default),
-  ja: () => import("./dictionaries/ja.json").then((module) => module.default),
-};
-
-export const getDictionary = async (locale: "en" | "ja") =>
-  dictionaries[locale]();
