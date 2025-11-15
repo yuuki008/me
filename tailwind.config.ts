f660be1c522b7c473d0600398f24b648c2dfa956
yuuki008 <s22ah261@gmail.com>
2025-11-15 18:13:56 +0900

    Add tailwindcssAnimate plugin to plugins array in tailwind.config.ts file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import tailwindcssAnimate from "tailwindcss-animate";
 
 export default {
   content: [
@@ -57,5 +58,5 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindcssAnimate],
 } satisfies Config;
