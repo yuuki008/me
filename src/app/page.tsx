691cf05a793bc342814c31aed799d41e7860955e
yuuki008 <s22ah261@gmail.com>
2025-11-15 10:51:20 +0900

    Refactor profile page layout and content in Page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,80 +1,41 @@
 "use client";
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-} from "@/components/ui/carousel";
 import Link from "next/link";
 import Image from "next/image";
-import AutoScroll from "embla-carousel-auto-scroll";
-
-const images = [
-  "/top/01.jpeg",
-  "/top/02.jpeg",
-  "/top/03.jpeg",
-  "/top/05.jpeg",
-  "/top/07.JPG",
-  "/top/08.JPG",
-  "/top/09.PNG",
-  "/top/10.JPG",
-  "/top/12.JPG",
-  "/top/13.JPG",
-  "/top/14.JPG",
-  "/top/15.JPG",
-  "/top/16.JPG",
-  "/top/17.JPG",
-  "/top/18.JPG",
-];
 
 export default function Page() {
   return (
-    <div className="py-10">
-      <div className="max-w-2xl px-2 mx-auto">
-        <div className="text-5xl font-bold">能村 優希</div>
+    <div className="flex items-center justify-center h-screen">
+      <div className="max-w-2xl mx-auto px-4 flex sm:gap-8 gap-4 sm:flex-row flex-col">
+        <Image
+          className="rounded-full border-4 w-[120px] h-[120px]"
+          src="/profile.jpeg"
+          alt="profile"
+          width={1000}
+          height={1000}
+        />
+        <div className="sm:py-6">
+          <div className="text-4xl font-bold tracking-wide">能村 優希</div>
+          <a
+            href="https://github.com/yuuki008"
+            className="text-sm font-semibold"
+          >
+            @yuuki008
+          </a>
 
-        <div className="mt-10 sm:text-base text-sm">
-          福岡でデータ基盤と Web サービスの開発・運用をしているエンジニアです。
-          <br />
-          休日は、ランニングや登山、トレーニング、読書をして過ごすことが多いです。シンプルで無駄のないものが大好きで、そういうものを追求していたいなと思っています。
-          <br />
-          <br />
-          最近ブログを書き始めました。
-          <Link className="text-blue-500" href="/blog">
-            こちら
-          </Link>
-          から気になる方は覗いてみてください。
+          <div className="mt-4 text-sm">
+            福岡でデータ基盤と Web
+            サービスの開発・運用をしているエンジニアです。
+            <br />
+            休日は、ランニングや登山、トレーニング、読書をして過ごすことが多いです。シンプルで無駄のないものが大好きで、そういうものを追求していたいなと思っています。
+            <br />
+            <br />
+            最近ブログを書き始めました。
+            <Link className="text-blue-500" href="/blog">
+              こちら
+            </Link>
+            から気になる方は覗いてみてください。
+          </div>
         </div>
-      </div>
-      <div className="mt-10">
-        <Carousel
-          className="w-full"
-          opts={{
-            loop: true,
-          }}
-          plugins={[
-            AutoScroll({
-              playOnInit: true,
-              direction: "forward",
-              speed: 0.8,
-            }),
-          ]}
-        >
-          <CarouselContent>
-            {images.map((image, index) => (
-              <CarouselItem className="basis-[75%] max-w-[300px]" key={index}>
-                <Image
-                  src={image}
-                  alt="top"
-                  width={10000}
-                  height={10000}
-                  className="w-full h-full max-h-[300px] object-cover"
-                  priority={index === 0}
-                  loading="eager"
-                />
-              </CarouselItem>
-            ))}
-          </CarouselContent>
-        </Carousel>
       </div>
     </div>
   );
