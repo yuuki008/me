6f9ec24935b32e77478f3508cc5362cdc41374b5
yuuki008 <s22ah261@gmail.com>
2025-11-16 23:02:15 +0900

    Update blog post title font size in page component

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -17,7 +17,7 @@ export default async function Blog({
 
   return (
     <section className="max-w-xl mx-auto px-4 py-8">
-      <h1 className="font-bold text-5xl text-center tracking-tighter">
+      <h1 className="font-bold sm:text-5xl text-4xl text-center tracking-tight">
         {post.title}
       </h1>
       <time className="text-secondary-foreground flex justify-center text-neutral-500 dark:text-neutral-400 mt-2">
