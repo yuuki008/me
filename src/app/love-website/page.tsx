b77943e626a14d4c357e2be6e8596739dc69128f
yuuki008 <s22ah261@gmail.com>
2025-12-02 14:20:57 +0900

    Update site list in Page component with additional link in a block format

--- src/app/love-website/page.tsx
+++ src/app/love-website/page.tsx
@@ -1,11 +1,14 @@
 import Link from "next/link"
 
 export default function Page() {
-  const sites = [{ url: "https://leonbubova.github.io/", title: 'leonbubova.github.io' }]
+  const sites = [
+    { url: "https://leonbubova.github.io/", title: 'leonbubova.github.io' },
+    { url: "http://bettermotherfuckingwebsite.com/", title: "bettermotherfuckingwebsite.com" },
+  ]
 
   return (
-    <div className="py-20 max-w-lg mx-auto">
-      {sites.map(site => <Link className="text-lg" key={site.url} href={site.url}>{site.title}</Link>)}
+    <div className="py-20 max-w-lg mx-auto space-y-4">
+      {sites.map(site => <Link className="block text-lg" key={site.url} href={site.url}>{site.title}</Link>)}
     </div>
   )
 }
