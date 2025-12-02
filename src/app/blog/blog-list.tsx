4487c342b546a0d1ff5afd477a70b61df616a026
yuuki008 <s22ah261@gmail.com>
2025-12-02 13:48:55 +0900

    Refactor blog list layout and text styles for better readability and consistency

--- src/app/blog/blog-list.tsx
+++ src/app/blog/blog-list.tsx
@@ -1,13 +1,18 @@
-import Link from "next/link";
 import { Post } from "./utils";
+import { CustomMDX } from "./[slug]/mdx";
 
 export function BlogList({ posts }: { posts: Post[] }) {
   return (
-    <div className="space-y-8 py-10 max-w-xl mx-auto">
+    <div className="divide-y py-14 max-w-lg mx-auto">
       {posts.map((post) => (
-        <Link className="block" key={post.slug} href={`/blog/${post.slug}`}>
-          <div className="text-xl tracking-wide font-semibold">{post.title}</div>
-        </Link>
+        <div className="py-10" key={post.slug}>
+          <div className="text-xl tracking-wide font-bold">{post.title}</div>
+          <time className="text-gray-500 text-sm pt-2">{post.date}</time>
+
+          <article className="prose mt-4">
+            <CustomMDX source={post.content} slug={post.slug} />
+          </article>
+        </div>
       ))}
     </div>
   );
