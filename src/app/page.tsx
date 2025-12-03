17385ec91a5610fb19c5f6bfea2b6511f2f7bbfe
yuuki008 <s22ah261@gmail.com>
2025-12-03 17:52:07 +0900

    Refactor profile section in Page component to remove Image component and update content structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,21 @@
 "use client";
 import Link from "next/link";
-import Image from "next/image";
 
 export default function Page() {
   return (
     <div className="flex items-center justify-center h-screen">
       <div className="px-4 max-w-lg mx-auto gap-4">
-        <Image
-          className="rounded-full w-[120px] h-[120px] object-cover"
-          src="/childhood02.jpg"
-          alt="profile"
-          width={1000}
-          height={1000}
-        />
-        <div className="mt-6">
-          <div className="text-4xl font-bold tracking-wide">能村 優希</div>
+        <div className="text-4xl font-bold tracking-wide">能村 優希</div>
 
-          <div className="mt-4 text-sm">
-            こんにちは。福岡でデータのお片付けやプログラムを書くお仕事をしています。
-            <br />
-            体をハードに動かすことに取り憑かれていて、今はランニングや登山、筋トレ、ヨガにハマっています。
-            <br />
-            <br />
-            シンプルで心地がよいもの好きです。
-            <br />
-            ブログは<Link className="text-blue-500" href="/blog">こちら</Link>からどうぞ。
-          </div>
+        <div className="mt-4 text-sm">
+          こんにちは。福岡でデータのお片付けやプログラムを書くお仕事をしています。
+          <br />
+          体をハードに動かすことに取り憑かれていて、今はランニングや登山、筋トレ、ヨガにハマっています。
+          <br />
+          <br />
+          シンプルで心地がよいもの好きです。
+          <br />
+          ブログは<Link className="text-blue-500" href="/blog">こちら</Link>からどうぞ。
         </div>
       </div>
     </div>
