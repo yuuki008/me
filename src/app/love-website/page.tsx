2bdb7d717f049307035c715e8560b44e258839ca
yuuki008 <s22ah261@gmail.com>
2025-12-04 12:07:31 +0900

    Add new website entry for yamatomichi in Page component

--- src/app/love-website/page.tsx
+++ src/app/love-website/page.tsx
@@ -5,7 +5,8 @@ export default function Page() {
     { url: "https://leonbubova.github.io/", title: 'leonbubova.github.io' },
     { url: "http://bettermotherfuckingwebsite.com/", title: "bettermotherfuckingwebsite.com" },
     { url: "https://blog.codinghorror.com/", title: "blog.codinghorror.com" },
-    { url: "https://kentbeck.com/", title: "kentbeck.com" }
+    { url: "https://kentbeck.com/", title: "kentbeck.com" },
+    { url: "https://www.yamatomichi.com/", title: "yamatomichi" }
   ]
 
   return (
