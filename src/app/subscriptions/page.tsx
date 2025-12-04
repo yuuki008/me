2589616e19902012e38de02d86d58d70ee20bed2
yuuki008 <s22ah261@gmail.com>
2025-12-04 17:54:45 +0900

    Update subscription role width and order in Page component

--- src/app/subscriptions/page.tsx
+++ src/app/subscriptions/page.tsx
@@ -18,7 +18,7 @@ function Page() {
       name: 'Youtube Premium',
       url: 'https://www.youtube.com/premium',
       price: '13.99 ドル',
-      role: "音楽と動画"
+      role: "動画と音楽"
     },
     {
       name: 'Amazon Prime',
@@ -45,10 +45,10 @@ function Page() {
     <div className="max-w-lg px-6 mx-auto h-screen flex flex-col justify-center items-center">
       <div className="text-5xl font-bold mb-14 tracking-wide">私のサブスク</div>
 
-      <div>
+      <div className="w-full">
         {subscriptions.map((sub, index) => (
-          <div key={index} className="flex items-center justify-between py-2 space-x-1">
-            <div className="w-[150px]">{sub.role}</div>
+          <div key={index} className="flex items-center justify-between py-2 space-x-1 w-full">
+            <div className="w-[130px]">{sub.role}</div>
             <Link href={sub.url} target="_blank" className="flex-1 text-left block text-blue-600 hover:underline">
               {sub.name}
             </Link>
