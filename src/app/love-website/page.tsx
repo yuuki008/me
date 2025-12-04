db90993d842926c95c5a79a65e31decc92a51003
yuuki008 <s22ah261@gmail.com>
2025-12-04 13:09:56 +0900

    Update link title and improve page layout for Love Website page

--- src/app/love-website/page.tsx
+++ src/app/love-website/page.tsx
@@ -6,12 +6,15 @@ export default function Page() {
     { url: "http://bettermotherfuckingwebsite.com/", title: "bettermotherfuckingwebsite.com" },
     { url: "https://blog.codinghorror.com/", title: "blog.codinghorror.com" },
     { url: "https://kentbeck.com/", title: "kentbeck.com" },
-    { url: "https://www.yamatomichi.com/", title: "yamatomichi" }
+    { url: "https://www.yamatomichi.com/", title: "yamatomichi.com" }
   ]
 
   return (
-    <div className="py-20 max-w-lg mx-auto space-y-4">
-      {sites.map(site => <Link className="block text-lg" key={site.url} href={site.url}>{site.title}</Link>)}
+    <div className="py-20 max-w-lg mx-auto">
+      <div className="text-5xl font-bold tracking-tight mb-8">Love Website</div>
+      <div className="space-y-4">
+        {sites.map(site => <Link className="block text-lg" key={site.url} href={site.url}>{site.title}</Link>)}
+      </div>
     </div>
   )
 }
