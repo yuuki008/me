ad8c934acd6a5ac5f71f64c63432cdddb516faa4
yuuki008 <s22ah261@gmail.com>
2025-12-04 13:57:28 +0900

    Update UI for Love Website and Subscriptions page with improved styles and content

--- src/app/subscriptions/page.tsx
+++ src/app/subscriptions/page.tsx
@@ -3,7 +3,6 @@ import Link from "next/link"
 type Subscription = {
   url: string,
   name: string,
-  plan?: string,
   price: string
   role: string
 }
@@ -11,50 +10,47 @@ function Page() {
   const subscriptions: Subscription[] = [
     {
       url: 'https://claude.ai',
-      name: 'Claude',
-      plan: 'Max',
+      name: 'Claude Max',
       price: '100 ドル',
-      role: "AI & Development"
+      role: "AI"
     },
     {
       name: 'Youtube Premium',
-      plan: 'Individual',
       url: 'https://www.youtube.com/premium',
       price: '13.99 ドル',
-      role: "Music & Video"
-    },
-    {
-      name: '1 Password',
-      price: '2.39 ドル',
-      plan: 'Individual',
-      url: 'https://1password.com/',
-      role: "Password Manager"
+      role: "音楽と動画"
     },
     {
       name: 'Amazon Prime',
       url: 'https://www.amazon.co.jp/-/en/amazonprime',
       price: '600 円',
-      role: "Video & EC"
+      role: "動画とEC"
     },
-
+    {
+      name: '1 Password',
+      price: '2.39 ドル',
+      url: 'https://1password.com/',
+      role: "パスワード"
+    },
     {
       name: 'Anytime Fitness',
       price: '7,480 円',
       url: 'https://www.anytimefitness.co.jp/',
-      role: "Fitness"
+      role: "フィットネス"
     }
   ]
 
 
   return (
-    <div className="py-20 max-w-lg mx-auto">
-      <div className="text-5xl font-bold tracking-tight mb-8">Subscription</div>
+    <div className="max-w-lg px-6 mx-auto h-screen flex flex-col justify-center items-center">
+      <div className="text-5xl font-bold mb-14 tracking-wide">私のサブスク</div>
 
       <div>
         {subscriptions.map((sub, index) => (
-          <div key={index} className="border-b flex items-center justify-between py-2">
-            <Link href={sub.url} target="_blank" className="block text-blue-600 hover:underline">
-              {sub.name}{sub.plan ? ` (${sub.plan})` : ''}
+          <div key={index} className="flex items-center justify-between py-2 space-x-1">
+            <div className="w-[150px]">{sub.role}</div>
+            <Link href={sub.url} target="_blank" className="flex-1 text-left block text-blue-600 hover:underline">
+              {sub.name}
             </Link>
             <div className="text-sm">{sub.price}</div>
           </div>
